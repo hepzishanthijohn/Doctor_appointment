e5f31002170a135a90b06c82f121ec986875eb76
hepzishanthijohn <yyepsipal@gmail.com>
2024-10-22 16:30:40 +0530

    update

--- frontend/src/pages/appointment/online/OnlineConsultation.jsx
+++ frontend/src/pages/appointment/online/OnlineConsultation.jsx
@@ -1,12 +1,15 @@
 // src/pages/OnlineConsultation.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import doc1 from '../../../assets/images/doc1.png'
+import doc2 from '../../../assets/images/doc2.png'
+import doc3 from '../../../assets/images/doc4.png'
 import './OnlineConsultation.css';
 
 const doctors = [
-    { id: 1, name: 'Dr. John Doe', specialty: 'Cardiology', contact: 'john@example.com' },
-    { id: 2, name: 'Dr. Jane Smith', specialty: 'Dermatology', contact: 'jane@example.com' },
-    { id: 3, name: 'Dr. Emily Johnson', specialty: 'Pediatrics', contact: 'emily@example.com' },
+    { id: 1, name: 'Dr. John Doe',img: doc1, specialty: 'Cardiology', contact: 'john@example.com' },
+    { id: 2, name: 'Dr. Jane Smith',img: doc2, specialty: 'Dermatology', contact: 'jane@example.com' },
+    { id: 3, name: 'Dr. Emily Johnson',img: doc3, specialty: 'Pediatrics', contact: 'emily@example.com' },
 ];
 
 const OnlineConsultation = () => {
@@ -18,6 +21,7 @@ const OnlineConsultation = () => {
             <div className="doctor-list">
                 {doctors.map((doctor) => (
                     <div className="doctor-card" key={doctor.id}>
+                        <img src={doctor.img} alt="" />
                         <h3>{doctor.name}</h3>
                         <p>Specialty: {doctor.specialty}</p>
                         <p>Contact: {doctor.contact}</p>
